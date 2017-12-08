3a30eaa4ea6e427c8d01c33e5245543f231d4365
jokedurnez <joke.durnez@gmail.com>
2017-12-08 13:59:50 -0800

    adapt to pybids 0.4.2 + add checks for consistency

--- run.py
+++ run.py
@@ -252,6 +252,8 @@ if args.analysis_level == "participant":
                      "seunwarpdir": "NONE"}
 
         if fieldmap_set:
+            fieldmap_trans = dict(zip(fieldmap_set[0],zip(*[d.values() for d in fieldmap_set])))
+
             t1_spacing = layout.get_metadata(t1ws[0])["EffectiveEchoSpacing"]
             t2_spacing = layout.get_metadata(t2ws[0])["EffectiveEchoSpacing"]
 
@@ -264,57 +266,66 @@ if args.analysis_level == "participant":
                               "t2samplespacing": "%.8f"%t2_spacing,
                               "unwarpdir": unwarpdir})
 
-            if fieldmap_set["type"] == "phasediff":
+            if set(fieldmap_trans['type'])==set(['phasediff']):
                 merged_file = "%s/tmp/%s/magfile.nii.gz"%(args.output_dir, subject_label)
                 run("mkdir -p %s/tmp/%s/ && fslmerge -t %s %s %s"%(args.output_dir,
                 subject_label,
                 merged_file,
-                fieldmap_set["magnitude1"],
-                fieldmap_set["magnitude2"]))
+                fieldmap_trans["magnitude1"],
+                fieldmap_trans["magnitude2"]))
 
-                phasediff_metadata = layout.get_metadata(fieldmap_set["phasediff"])
+                phasediff_metadata = layout.get_metadata(fieldmap_trans["phasediff"])
                 te_diff = phasediff_metadata["EchoTime2"] - phasediff_metadata["EchoTime1"]
                 # HCP expects TE in miliseconds
                 te_diff = te_diff*1000.0
 
                 fmap_args.update({"fmapmag": merged_file,
-                                  "fmapphase": fieldmap_set["phasediff"],
+                                  "fmapphase": fieldmap_trans["phasediff"],
                                   "echodiff": "%.6f"%te_diff,
                                   "avgrdcmethod": "SiemensFieldMap"})
-            elif fieldmap_set["type"] == "epi":
+
+            elif set(fieldmap_trans['type'])== set(["epi"]):
                 SEPhaseNeg = None
                 SEPhasePos = None
-                for fieldmap in fieldmap_set["epi"]:
+                seunwarpdir = None
+                for fieldmap in fieldmap_trans["epi"]:
                     enc_dir = layout.get_metadata(fieldmap)["PhaseEncodingDirection"]
                     if "-" in enc_dir:
                         SEPhaseNeg = fieldmap
                     else:
                         SEPhasePos = fieldmap
 
-                seunwarpdir = layout.get_metadata(fieldmap_set["epi"][0])["PhaseEncodingDirection"]
-                seunwarpdir = seunwarpdir.replace("-", "").replace("i","x").replace("j", "y").replace("k", "z")
+                unwarpdir = enc_dir.replace("-", "").replace("i","x").replace("j", "y").replace("k", "z")
+                if seunwarpdir and not seunwarpdir == unwarpdir:
+                    raise RuntimeError("Inconsistent unwarp directions.")
 
-                #TODO check consistency of echo spacing instead of assuming it's all the same
-                if "EffectiveEchoSpacing" in layout.get_metadata(fieldmap_set["epi"][0]):
-                    echospacing = layout.get_metadata(fieldmap_set["epi"][0])["EffectiveEchoSpacing"]
-                elif "TotalReadoutTime" in layout.get_metadata(fieldmap_set["epi"][0]):
-                    # HCP Pipelines do not allow users to specify total readout time directly
-                    # Hence we need to reverse the calculations to provide echo spacing that would
-                    # result in the right total read out total read out time
-                    # see https://github.com/Washington-University/Pipelines/blob/master/global/scripts/TopupPreprocessingAll.sh#L202
-                    print("BIDS App wrapper: Did not find EffectiveEchoSpacing, calculating it from TotalReadoutTime")
-                    # TotalReadoutTime = EffectiveEchoSpacing * (len(PhaseEncodingDirection) - 1)
-                    total_readout_time = layout.get_metadata(fieldmap_set["epi"][0])["TotalReadoutTime"]
-                    phase_len = nibabel.load(fieldmap_set["epi"][0]).shape[{"x": 0, "y": 1}[seunwarpdir]]
-                    echospacing = total_readout_time / float(phase_len - 1)
-                else:
-                    raise RuntimeError("EffectiveEchoSpacing or TotalReadoutTime not defined for the fieldmap intended for T1w image. Please fix your BIDS dataset.")
+                fmap_arguments = {}
+                for fieldmap in fieldmap_set:
+                    if "EffectiveEchoSpacing" in layout.get_metadata(fieldmap["epi"]):
+                        echospacing = layout.get_metadata(fieldmap["epi"])["EffectiveEchoSpacing"]
+                    elif "TotalReadoutTime" in layout.get_metadata(fieldmap["epi"]):
+                        # HCP Pipelines do not allow users to specify total readout time directly
+                        # Hence we need to reverse the calculations to provide echo spacing that would
+                        # result in the right total read out total read out time
+                        # see https://github.com/Washington-University/Pipelines/blob/master/global/scripts/TopupPreprocessingAll.sh#L202
+                        print("BIDS App wrapper: Did not find EffectiveEchoSpacing, calculating it from TotalReadoutTime")
+                        # TotalReadoutTime = EffectiveEchoSpacing * (len(PhaseEncodingDirection) - 1)
+                        total_readout_time = layout.get_metadata(fieldmap["epi"])["TotalReadoutTime"]
+                        phase_len = nibabel.load(fieldmap["epi"]).shape[{"x": 0, "y": 1}[seunwarpdir]]
+                        echospacing = total_readout_time / float(phase_len - 1)
+                    else:
+                        raise RuntimeError("EffectiveEchoSpacing or TotalReadoutTime not defined for the fieldmap intended for T1w image. Please fix your BIDS dataset.")
+                    if 'echospacing' in fmap_arguments.keys() and not fmap_arguments['echospacing']==echospacing:
+                        raise RuntimeError("Inconsistent echospacing.")
 
                 fmap_args.update({"SEPhaseNeg": SEPhaseNeg,
                                   "SEPhasePos": SEPhasePos,
                                   "echospacing": "%.6f"%echospacing,
                                   "seunwarpdir": seunwarpdir,
                                   "avgrdcmethod": "TOPUP"})
+            else:
+                raise RuntimeError("Inconsistent fieldmap types or unknown type.")
+
         #TODO add support for GE fieldmaps
 
         struct_stages_dict = OrderedDict([("PreFreeSurfer", partial(run_pre_freesurfer,
@@ -352,30 +363,32 @@ if args.analysis_level == "participant":
             if not os.path.exists(fmriscout):
                 fmriscout = "NONE"
 
-            fieldmap_set = layout.get_fieldmap(fmritcs)
-            if fieldmap_set and fieldmap_set["type"] == "epi":
-                SEPhaseNeg = None
-                SEPhasePos = None
-                for fieldmap in fieldmap_set["epi"]:
-                    enc_dir = layout.get_metadata(fieldmap)["PhaseEncodingDirection"]
-                    if "-" in enc_dir:
-                        SEPhaseNeg = fieldmap
-                    else:
-                        SEPhasePos = fieldmap
-                echospacing = layout.get_metadata(fmritcs)["EffectiveEchoSpacing"]
-                unwarpdir = layout.get_metadata(fmritcs)["PhaseEncodingDirection"]
-                unwarpdir = unwarpdir.replace("i","x").replace("j", "y").replace("k", "z")
-                if len(unwarpdir) == 2:
-                    unwarpdir = "-" + unwarpdir[0]
-                dcmethod = "TOPUP"
-                biascorrection = "SEBASED"
-            else:
-                SEPhaseNeg = "NONE"
-                SEPhasePos = "NONE"
-                echospacing = "NONE"
-                unwarpdir = "NONE"
-                dcmethod = "NONE"
-                biascorrection = "NONE"
+            fieldmap_set = layout.get_fieldmap(fmritcs,return_list=True)
+            if fieldmap_set:
+                fieldmap_trans = dict(zip(fieldmap_set[0],zip(*[d.values() for d in fieldmap_set])))
+                if set(fieldmap_trans['type'])== set(["epi"]):
+                    SEPhaseNeg = None
+                    SEPhasePos = None
+                    for fieldmap in fieldmap_set:
+                        enc_dir = layout.get_metadata(fieldmap['epi'])["PhaseEncodingDirection"]
+                        if "-" in enc_dir:
+                            SEPhaseNeg = fieldmap['epi']
+                        else:
+                            SEPhasePos = fieldmap['epi']
+                    echospacing = layout.get_metadata(fmritcs)["EffectiveEchoSpacing"]
+                    unwarpdir = layout.get_metadata(fmritcs)["PhaseEncodingDirection"]
+                    unwarpdir = unwarpdir.replace("i","x").replace("j", "y").replace("k", "z")
+                    if len(unwarpdir) == 2:
+                        unwarpdir = "-" + unwarpdir[0]
+                    dcmethod = "TOPUP"
+                    biascorrection = "SEBASED"
+                else:
+                    SEPhaseNeg = "NONE"
+                    SEPhasePos = "NONE"
+                    echospacing = "NONE"
+                    unwarpdir = "NONE"
+                    dcmethod = "NONE"
+                    biascorrection = "NONE"
 
             zooms = nibabel.load(fmritcs).get_header().get_zooms()
             fmrires = float(min(zooms[:3]))
