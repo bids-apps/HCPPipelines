dd7200fe658268a26e7442f41b18f6e16f03a5fb
jokedurnez <joke.durnez@gmail.com>
2018-03-12 12:15:49 -0700

    add diffusion pipeline

--- run.py
+++ run.py
@@ -4,7 +4,6 @@ import argparse
 import os
 import shutil
 import nibabel
-import copy
 from glob import glob
 from subprocess import Popen, PIPE
 from shutil import rmtree
@@ -64,6 +63,7 @@ def run_pre_freesurfer(**args):
     '--t2samplespacing="{t2samplespacing}" ' + \
     '--unwarpdir="{unwarpdir}" ' + \
     '--gdcoeffs="NONE" ' + \
+    '--extra-eddy-arg="--data_is_shelled"' + \
     '--avgrdcmethod={avgrdcmethod} ' + \
     '--topupconfig="{HCPPIPEDIR_Config}/b02b0.cnf" '
     cmd = cmd.format(**args)
@@ -158,14 +158,13 @@ def run_diffusion_processsing(**args):
       '--subject="{subject}" ' + \
       '--echospacing="{echospacing}" '+ \
       '--PEdir={PEdir} ' + \
-      '--gdcoeffs="NONE" ' + \
-      '--extra-eddy-arg="--data_is_shelled" '
+      '--gdcoeffs="NONE" '
     cmd = cmd.format(**args)
     run(cmd, cwd=args["path"], env={"OMP_NUM_THREADS": str(args["n_cpus"])})
 
 __version__ = open('/version').read()
 
-parser = argparse.ArgumentParser(description='HCP Pipelines BIDS App (T1w, T2w, fMRI, DWI)')
+parser = argparse.ArgumentParser(description='HCP Pipeliens BIDS App (T1w, T2w, fMRI, DWI)')
 parser.add_argument('bids_dir', help='The directory with the input dataset '
                     'formatted according to the BIDS standard.')
 parser.add_argument('output_dir', help='The directory where the output files '
@@ -191,6 +190,8 @@ parser.add_argument('--stages', help='Which stages to run. Space separated list.
                    default=['PreFreeSurfer', 'FreeSurfer', 'PostFreeSurfer',
                             'fMRIVolume', 'fMRISurface',
                             'DiffusionPreprocessing'])
+parser.add_argument('--license_key', help='FreeSurfer license key - letters and numbers after "*" in the email you received after registration. To register (for free) visit https://surfer.nmr.mgh.harvard.edu/registration.html',
+                    required=True)
 parser.add_argument('-v', '--version', action='version',
                     version='HCP Pipelines BIDS App version {}'.format(__version__))
 
@@ -246,7 +247,7 @@ if args.analysis_level == "participant":
 
         if fieldmap_set:
             if len(fieldmap_set)>1:
-                fieldmap_trans = dict(zip(fieldmap_set[0],zip(*[d.values() for d in fieldmap_set])))
+                fieldmap_trans=dict(zip(d,zip(*[d.values() for d in fieldmap_set])))
             else:
                 fieldmap_trans = {k:[v] for k,v in fieldmap_set[0].iteritems()}
 
@@ -262,7 +263,7 @@ if args.analysis_level == "participant":
                               "t2samplespacing": "%.8f"%t2_spacing,
                               "unwarpdir": unwarpdir})
 
-            if set(fieldmap_trans['type'])==set(['phasediff']):
+            if set(fieldmap_set["type"]) == set(["phasediff"]):
                 merged_file = "%s/tmp/%s/magfile.nii.gz"%(args.output_dir, subject_label)
                 run("mkdir -p %s/tmp/%s/ && fslmerge -t %s %s %s"%(args.output_dir,
                 subject_label,
@@ -279,8 +280,7 @@ if args.analysis_level == "participant":
                                   "fmapphase": fieldmap_trans["phasediff"][0],
                                   "echodiff": "%.6f"%te_diff,
                                   "avgrdcmethod": "SiemensFieldMap"})
-
-            elif set(fieldmap_trans['type'])== set(["epi"]):
+            elif set(fieldmap_set["type"]) == set(["epi"]):
                 SEPhaseNeg = None
                 SEPhasePos = None
                 seunwarpdir = None
@@ -291,40 +291,39 @@ if args.analysis_level == "participant":
                     else:
                         SEPhasePos = fieldmap
 
-                unwarpdir = enc_dir.replace("-", "").replace("i","x").replace("j", "y").replace("k", "z")
+                unwarpdir = enc_dir.replace('-', '').replace('i','x').replace('j','y').replace('k',z)
                 if seunwarpdir and not seunwarpdir == unwarpdir:
                     raise RuntimeError("Inconsistent unwarp directions.")
                 else:
-                    seunwarpdir = copy.deepcopy(unwarpdir)
+                    raise RuntimeError("EffectiveEchoSpacing or TotalReadoutTime not defined for the fieldmap intended for T1w.  Please fix your dataset.")
+                seunwarpdir = copy.deepcopy(unwarpdir)
 
                 fmap_arguments = {}
                 for fieldmap in fieldmap_set:
-                    if "EffectiveEchoSpacing" in layout.get_metadata(fieldmap["epi"]):
-                        echospacing = layout.get_metadata(fieldmap["epi"])["EffectiveEchoSpacing"]
-                    elif "TotalReadoutTime" in layout.get_metadata(fieldmap["epi"]):
+                    if "EffectiveEchoSpacing" in layout.get_metadata(fieldmap_set["epi"]):
+                        echospacing = layout.get_metadata(fieldmap_set["epi"])["EffectiveEchoSpacing"]
+                    elif "TotalReadoutTime" in layout.get_metadata(fieldmap_set["epi"]):
                         # HCP Pipelines do not allow users to specify total readout time directly
                         # Hence we need to reverse the calculations to provide echo spacing that would
                         # result in the right total read out total read out time
                         # see https://github.com/Washington-University/Pipelines/blob/master/global/scripts/TopupPreprocessingAll.sh#L202
                         print("BIDS App wrapper: Did not find EffectiveEchoSpacing, calculating it from TotalReadoutTime")
                         # TotalReadoutTime = EffectiveEchoSpacing * (len(PhaseEncodingDirection) - 1)
-                        total_readout_time = layout.get_metadata(fieldmap["epi"])["TotalReadoutTime"]
-                        phase_len = nibabel.load(fieldmap["epi"]).shape[{"x": 0, "y": 1}[seunwarpdir]]
+                        total_readout_time = layout.get_metadata(fieldmap_set["epi"])["TotalReadoutTime"]
+                        phase_len = nibabel.load(fieldmap_set["epi"]).shape[{"x": 0, "y": 1}[seunwarpdir]]
                         echospacing = total_readout_time / float(phase_len - 1)
-                    else:
-                        raise RuntimeError("EffectiveEchoSpacing or TotalReadoutTime not defined for the fieldmap intended for T1w image. Please fix your BIDS dataset.")
-                    if 'echospacing' in fmap_arguments.keys() and not fmap_arguments['echospacing']==echospacing:
-                        raise RuntimeError("Inconsistent echospacing.")
+                else:
+                    raise RuntimeError("EffectiveEchoSpacing or TotalReadoutTime not defined for the fieldmap intended for T1w image. Please fix your BIDS dataset.")
+                if 'echospacing' in fmap_arguments.keys() and not fmap_arguments['echospacing']==echospacing:
+                    raise RuntimeError("Inconsistent echospacing.")
 
                 fmap_args.update({"SEPhaseNeg": SEPhaseNeg,
                                   "SEPhasePos": SEPhasePos,
                                   "echospacing": "%.6f"%echospacing,
                                   "seunwarpdir": seunwarpdir,
                                   "avgrdcmethod": "TOPUP"})
-            else:
-                raise RuntimeError("Inconsistent fieldmap types or unknown type.")
-
-        #TODO add support for GE fieldmaps
+        else:
+            raise RuntimeError("Inconsistent fieldmap types or unknown type.")
 
         struct_stages_dict = OrderedDict([("PreFreeSurfer", partial(run_pre_freesurfer,
                                                 path=args.output_dir,
@@ -364,29 +363,29 @@ if args.analysis_level == "participant":
             fieldmap_set = layout.get_fieldmap(fmritcs,return_list=True)
             if fieldmap_set:
                 fieldmap_trans = dict(zip(fieldmap_set[0],zip(*[d.values() for d in fieldmap_set])))
-                if set(fieldmap_trans['type'])== set(["epi"]):
+                if set(fieldmap_trans['type']) == set(['epi']):
                     SEPhaseNeg = None
                     SEPhasePos = None
-                    for fieldmap in fieldmap_set:
-                        enc_dir = layout.get_metadata(fieldmap['epi'])["PhaseEncodingDirection"]
+                    for fieldmap in fieldmap_set["epi"]:
+                        enc_dir = layout.get_metadata(fieldmap)["PhaseEncodingDirection"]
                         if "-" in enc_dir:
-                            SEPhaseNeg = fieldmap['epi']
+                            SEPhaseNeg = fieldmap
                         else:
-                            SEPhasePos = fieldmap['epi']
-                    echospacing = layout.get_metadata(fmritcs)["EffectiveEchoSpacing"]
-                    unwarpdir = layout.get_metadata(fmritcs)["PhaseEncodingDirection"]
-                    unwarpdir = unwarpdir.replace("i","x").replace("j", "y").replace("k", "z")
-                    if len(unwarpdir) == 2:
-                        unwarpdir = "-" + unwarpdir[0]
-                    dcmethod = "TOPUP"
-                    biascorrection = "SEBASED"
-                else:
-                    SEPhaseNeg = "NONE"
-                    SEPhasePos = "NONE"
-                    echospacing = "NONE"
-                    unwarpdir = "NONE"
-                    dcmethod = "NONE"
-                    biascorrection = "NONE"
+                            SEPhasePos = fieldmap
+                echospacing = layout.get_metadata(fmritcs)["EffectiveEchoSpacing"]
+                unwarpdir = layout.get_metadata(fmritcs)["PhaseEncodingDirection"]
+                unwarpdir = unwarpdir.replace("i","x").replace("j", "y").replace("k", "z")
+                if len(unwarpdir) == 2:
+                    unwarpdir = "-" + unwarpdir[0]
+                dcmethod = "TOPUP"
+                biascorrection = "SEBASED"
+            else:
+                SEPhaseNeg = "NONE"
+                SEPhasePos = "NONE"
+                echospacing = "NONE"
+                unwarpdir = "NONE"
+                dcmethod = "NONE"
+                biascorrection = "NONE"
 
             zooms = nibabel.load(fmritcs).get_header().get_zooms()
             fmrires = float(min(zooms[:3]))
