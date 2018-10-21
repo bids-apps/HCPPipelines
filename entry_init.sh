5a8be68b100d057749715774a374c7c770705200
Monica Li <monica.yc.li@gmail.com>
2018-10-21 01:04:25 -0400

    add welcome message

--- entry_init.sh
+++ entry_init.sh
@@ -5,5 +5,8 @@ source /environment
 # make symlink to powerline-shell config
 ln -sf /powerline-shell.json $HOME/.powerline-shell.json
 
+# welcom message
+echo "Welcome to the BIRC's container for the Human Connectome Project (HCP) pipelines! "
+
 # run the user command
 exec "$@"
