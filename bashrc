ae28670a08f607835d8a32b4e70f6dcc8ac658eb
Monica Li <monica.yc.li@gmail.com>
2018-10-22 21:55:25 -0400

    move welcome message to bashrc

--- bashrc
+++ bashrc
@@ -5,3 +5,7 @@ function _update_ps1() {
 if [[ $TERM != linux && ! $PROMPT_COMMAND =~ _update_ps1 ]]; then
     PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"
 fi
+
+# welcom message
+echo "Welcome to the BIRC's container for the Human Connectome Project (HCP) pipelines! "
+
--- entry_init.sh
+++ entry_init.sh
@@ -5,8 +5,5 @@ source /environment
 # make symlink to powerline-shell config
 ln -sf /powerline-shell.json $HOME/.powerline-shell.json
 
-# welcom message
-echo "Welcome to the BIRC's container for the Human Connectome Project (HCP) pipelines! "
-
 # run the user command
 exec "$@"
